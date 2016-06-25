900d7185f0f2ea58921052b2396f994b533b0a10
ExTEnS10N <sunx123x@126.com>
2016-06-25 15:31:04 +0800

    修复随机打乱对Emoji的支持

--- js/Decrypt.js
+++ js/Decrypt.js
@@ -73,16 +73,13 @@ function utf8to16(str) {
         c = str.charCodeAt(i++);  
         switch(c >> 4) {  
             case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:  
-                // 0xxxxxxx  
                 out += str.charAt(i-1);  
                 break;  
-            case 12: case 13:  
-                // 110x xxxx 10xx xxxx  
+            case 12: case 13: 
                 char2 = str.charCodeAt(i++);  
                 out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));  
                 break;  
-            case 14:  
-                // 1110 xxxx 10xx xxxx 10xx xxxx  
+            case 14:
                 char2 = str.charCodeAt(i++);  
                 char3 = str.charCodeAt(i++);  
                 out += String.fromCharCode(((c & 0x0F) << 12) |  
