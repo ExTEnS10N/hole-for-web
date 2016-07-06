e72f9d318374fdc84ef13d32c533717c8fddd6b1
ExTEnS10N <sunx123x@126.com>
2016-07-06 16:32:40 +0800

    修复摩尔斯电码和少量bug，更换整站压缩工具为s.press

--- js/mode-ecb-min.js
+++ js/mode-ecb-min.js
@@ -1,7 +1,1 @@
-/*
-CryptoJS v3.1.2
-code.google.com/p/crypto-js
-(c) 2009-2013 by Jeff Mott. All rights reserved.
-code.google.com/p/crypto-js/wiki/License
-*/
-CryptoJS.mode.ECB=function(){var a=CryptoJS.lib.BlockCipherMode.extend();a.Encryptor=a.extend({processBlock:function(a,b){this._cipher.encryptBlock(a,b)}});a.Decryptor=a.extend({processBlock:function(a,b){this._cipher.decryptBlock(a,b)}});return a}();
+CryptoJS.mode.ECB=function(){var b=CryptoJS.lib.BlockCipherMode.extend();b.Encryptor=b.extend({processBlock:function(a,c){this._cipher.encryptBlock(a,c)}});b.Decryptor=b.extend({processBlock:function(a,c){this._cipher.decryptBlock(a,c)}});return b}();
